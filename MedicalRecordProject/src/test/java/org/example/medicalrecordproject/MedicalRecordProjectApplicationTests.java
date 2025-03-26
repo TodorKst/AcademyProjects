79140b1441a50b8c3f2efb18d55d96d380cc64e8
TodorKst <166815015+TodorKst@users.noreply.github.com>
2025-03-26 16:18:51 +0200

    created the base for the HospitalRecordProject

---
+++ MedicalRecordProject/src/test/java/org/example/medicalrecordproject/MedicalRecordProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.medicalrecordproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalRecordProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
